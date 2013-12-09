c6efc4bd8f8e0f507647ae75b896d5977d953c95
Kopter Z <kopter@gmail.com>
2013-12-08 17:53:33 -0800

    Checkpoint: Fixed plugin location

--- wordpress/wp-content/plugins/js/sortable.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function() {
-	jQuery("#sortable").sortable({
-		stop: function(event, ui) {
-			jQuery("#social_stickers_order").val(jQuery(this).sortable('serialize'));
-		}
-	});
-});
